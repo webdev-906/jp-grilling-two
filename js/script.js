79389807733f0ee86db7fa69de6a223ff00b1a8d
Alannah Gregory <alannahgregory@AdminisatorsMBP.lan>
2022-08-28 21:59:33 -0400

    Initial folder uploads

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// a function called my alert that will alert the user that the page was not found. It does this by adding a eventListener called 
+// onclick to any element that you call our function on
+function myAlert() {
+  alert("404 Error - Page not Found")
+}
+;
